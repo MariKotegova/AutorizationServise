d4d5723ff18367a0d95cf0b6f5d52328be6b36e2
MariKotegova <m.kotegowa@yandex.ru>
2023-03-27 17:55:46 +0300

    Добавлен тест

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:8-jdk-alpine
 
-EXPOSE 8080
+EXPOSE 8081
 
-ADD build/libs/AutorizationServise-0.0.1-SNAPSHOT.jar app.jar
+ADD build/libs/AutorizationServise-0.0.1-SNAPSHOT.jar myapp.jar
 
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "myapp.jar"]
--- src/test/java/ru/netology/autorizationservise/AutorizationServiseApplicationTests.java
+++ src/test/java/ru/netology/autorizationservise/AutorizationServiseApplicationTests.java
@@ -1,13 +1,36 @@
 package ru.netology.autorizationservise;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AutorizationServiseApplicationTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static GenericContainer<?> myAppDev = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    private static GenericContainer<?> myAppProd = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp()  {
+        myAppDev.start();
+        myAppProd.start();
+    }
 
     @Test
     void contextLoads() {
-    }
+        ResponseEntity<String> forEntityDev = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080), String.class);
+        ResponseEntity<String> forEntityProd = restTemplate.getForEntity("http://localhost:" + myAppProd.getMappedPort(8081), String.class);
 
+        System.out.println(forEntityDev.getBody());
+        System.out.println(forEntityProd.getBody());
+    }
 }
